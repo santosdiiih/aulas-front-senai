25cf5a084c669b2772d7d75019b7ea6703c74e2b
santosdiiih <aparecida.rpsilva@gmail.com>
2020-05-25 22:55:58 -0300

    formatacao do superHeroApi

--- superHero/script.js
+++ superHero/script.js
@@ -1,24 +1,49 @@
 const personId = 15;
-const getrawgio = async() => {
-    const url = `https://www.superheroapi.com/api.php/688375641946535/${personId}`;
-    const getApi = await fetch(url);
-    const json = await getApi.json();
+const getrawgio = async(name, image, aliancas) => {
 
+    const panel = document.createElement('div');
 
-    const panel = `
-    <div id="card"> 
-        <div id="nome"> 
-                ${json.name} 
+    panel.innerHTML += `
+    <div class="card"> 
+        <div class="nome"> 
+                ${name} 
         </div>
-        <div id="aliancas"> 
-            <img src="${json.image.url}" 
+        <div class="image"> 
+            <img src="${image}" 
+        </div>
+        <div class="biografia"> 
+            <p> Alianças com herois:  
+                 ${aliancas};
+                 
+            </p>
         </div>
     </div>
 `;
-    console.log(json.image.url);
+    // console.log(json.biography.aliases[0, 1, 2]);
 
     const $conteudo = document.querySelector('#conteudo');
-    $conteudo.innerHTML = panel;
+    $conteudo.appendChild(panel);
 
 }
-getrawgio();
+
+
+const exibeDados = async() => {
+    for (let i = 1; i < 33; i++) {
+        const url = `https://www.superheroapi.com/api.php/688375641946535/${i}`;
+        const getApi = await fetch(url);
+        const json = await getApi.json();
+
+        var name = await json.name;
+        var image = await json.image.url;
+        var aliancas = await json.biography.aliases;
+
+        // console.log(name);
+        // console.log(aliancas);
+
+        getrawgio(name, image, aliancas);
+    }
+
+
+
+}
+exibeDados();
